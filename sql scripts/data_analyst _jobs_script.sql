5131ba4c9d27faf167dd87d551020969087274dd
CameronMatlock <cameronmatlock14@gmail.com>
2023-06-10 10:01:13 -0500

    Finished answers after review

--- sql scripts/data_analyst _jobs_script.sql
+++ sql scripts/data_analyst _jobs_script.sql
@@ -43,6 +43,7 @@ WHERE review_count BETWEEN 500 AND 1000;
 
 SELECT location AS state, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
 GROUP BY location
 ORDER BY avg_rating DESC;
 
@@ -50,49 +51,53 @@ ORDER BY avg_rating DESC;
 
 -- Q7 - Select unique job titles from the data_analyst_jobs table. How many are there?
 
-SELECT DISTINCT(title)
+SELECT COUNT(DISTINCT(title))
 FROM data_analyst_jobs;
 
 -- Answer - There are 881 distinct job titles posted.
 
 -- Q8 - How many unique job titles are there for California companies?
 
-SELECT DISTINCT(title), location
+SELECT COUNT(DISTINCT(title)), location
 FROM data_analyst_jobs
-WHERE location = 'CA';
+WHERE location = 'CA'
+GROUP BY location;
 
 -- Answer - There are 230 unique job titles posted in the state of California.
 
 -- Q9 - Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-SELECT DISTINCT(company), review_count, AVG(star_rating) AS average_star_rating
+SELECT company, AVG(star_rating) AS average_star_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000
-GROUP BY company, review_count;
+AND company IS NOT NULL
+GROUP BY company;
 
 -- Answer - There are 46 companies that have more than 5000 reviews across all locations.
 
 -- Q10 - Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
-SELECT DISTINCT(company), review_count, AVG(star_rating) AS average_star_rating
+SELECT company, AVG(star_rating) AS average_star_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000
-GROUP BY company, review_count
+AND company IS NOT NULL
+GROUP BY company
 ORDER BY average_star_rating DESC;
 
 -- Answer - The company with the highest average star rating across all locations with more than 5000 reviews is a tie between 6 companies. These companies are American Express, General Motors, Kaiser Permanente, Microsoft, Nike and Unilever. They all have an average star rating of 4.19 stars.
 
 -- Q11 - Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 
-SELECT title
+SELECT COUNT(DISTINCT(title))
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
+WHERE title iLIKE '%Analyst%';
 
--- Answer - There are a total of 1636 job titles that contain the word 'Analyst'.
+
+-- Answer - There are a total of 774 job titles that contain the word 'Analyst'.
 
 -- Q12 - How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
-SELECT title, UPPER(title)
+SELECT UPPER(title)
 FROM data_analyst_jobs
 WHERE UPPER(title) NOT LIKE '%ANALYST%'
 AND UPPER(title) NOT LIKE '%ANALYTICS%';
@@ -101,38 +106,35 @@ AND UPPER(title) NOT LIKE '%ANALYTICS%';
 
 -- Bonus Question Part 1 - You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
 
-SELECT DISTINCT(domain), COUNT(title) AS number_of_jobs
+SELECT domain, COUNT(title) AS number_of_jobs
 FROM data_analyst_jobs
-WHERE skill = 'SQL'
+WHERE skill LIKE '%SQL%'
 AND days_since_posting > 21
 GROUP BY domain;
 
 -- Bonus Question Part 2 - Disregard any postings where the domain is NULL.
 
-SELECT DISTINCT(domain), COUNT(title) AS number_of_jobs
+SELECT domain, COUNT(title) AS number_of_jobs
 FROM data_analyst_jobs
-WHERE skill = 'SQL'
+WHERE skill LIKE '%SQL%'
 AND days_since_posting > 21
 AND domain IS NOT NULL
 GROUP BY domain;
 
 -- Bonus Question Part 3 - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 
-SELECT DISTINCT(domain), COUNT(title) AS number_of_jobs
+SELECT domain, COUNT(title) AS number_of_jobs
 FROM data_analyst_jobs
-WHERE skill = 'SQL'
+WHERE skill LIKE '%SQL%'
 AND days_since_posting > 21
 AND domain IS NOT NULL
 GROUP BY domain
-ORDER BY COUNT(title) DESC;
+ORDER BY COUNT(title) DESC
+LIMIT 4;
 
 -- Bonus Question Part 4 - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 
--- Answer - The four industries with the most hard to fill jobs are Consulting and Buisness Services, Consumer Goods and Services, Computers and Electronics and Internet and Software.
--- Consulting and Buisness Services - 5 Jobs
--- Consumer Goods and Services - 2 Jobs
--- Computers and Electronics - 1 Jobs
--- Internet and Software - 1 Job
+-- Answer 
 
 
 
